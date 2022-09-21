d7e31ecdf18713ceb270920bed90021725184e2b
Sutania <113482923+Sutania@users.noreply.github.com>
2022-09-20 20:07:46 -0500

    Added Link to Heroku

--- index.php
+++ index.php
@@ -33,6 +33,10 @@ include 'includes/header.php'
 
 
 <button type="button" class="btn btn-dark"> Click Me!</button>
+<button type="button" class="btn btn-primary"> Click Me!</button>
+<button type="button" class="btn btn-success"> Click Me!</button>
+
+<a href = "https://www.heroku.com" target = "_blank" class="btn btn-danger" > Heroku.com </a>
 
 
 
