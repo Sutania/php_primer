67615bdc78948263211a879ca6e8b8b76872640d
Sutania <113482923+Sutania@users.noreply.github.com>
2022-10-17 16:11:26 -0500

    Added freedb mysql connection details

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ include 'includes/header.php'
 <button type="button" class="btn btn-primary"> Click Me!</button>
 <button type="button" class="btn btn-success"> Click Me!</button>
 
-<a href = "https://www.heroku.com" target = "_blank" class="btn btn-danger" > Heroku.com </a>
+<a href = "https://www.heroku.com" target = "_blank"class="btn btn-danger" > Heroku.com </a>
 
 
 
